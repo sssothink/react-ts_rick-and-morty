cab707529577b941e6cb16ee3cd5da42fad7452b
Sothink <rebenkov2002@mail.ru>
2024-09-20 01:34:13 +0300

    feat: create environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import HeaderComponent from "./components/HeaderComponents/HeaderComponent";
+import MainComponent from "./components/MainComponents/MainComponent";
+import FooterComponent from "./components/FooterComponents/FooterComponent";
 
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<div className="app">
+				<HeaderComponent />
+				<MainComponent />
+				<FooterComponent />
+			</div>
+			;
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/MainComponents/MainComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainComponent = () => {
+	return <div></div>;
+};
+
+export default MainComponent;
